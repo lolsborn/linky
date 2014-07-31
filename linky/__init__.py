31f525228c3374dd5420f2588db772738842b8cc
Steven Osborn <osborn.steven@gmail.com>
2014-07-31 16:00:41 -0700

    initial

---
+++ linky/__init__.py
@@ -0,0 +1,1 @@
+from linky import linky
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Linky',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Steven Osborn',
+      author_email='steven@truly.email',
+      url='http://github.com/steve918/linky/',
+      packages=['linky'],
+     )
